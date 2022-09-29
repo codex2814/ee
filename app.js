0586bd7790d5d370b923633326ef9fffa1ff2161
codex2814 <114537636+codex2814@users.noreply.github.com>
2022-09-29 19:01:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+//declearing html elements
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+//if user hover on img div
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+//if we hover out from img div
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+//lets work for image showing functionality when we choose an image to upload
+
+//when we choose a foto to upload
+
+file.addEventListener('change', function(){
+    //this refers to file
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); //FileReader is a predefined function of JS
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+
+        //Allright is done
+
+        //please like the video
+        //comment if have any issue related to vide & also rate my work in comment section
+
+        //And aslo please subscribe for more tutorial like this
+
+        //thanks for watching
+    }
+});
+function btn(){
+    alert("working");
+    var image = document.querySelector('.img').src;
+    location.href = 'https://wa.me/+2347045649462?text='+image+'Hello,%20my%20name%20is%20and%20i%20want%20to%20sew%20of%20this%20native';
+  
+}
