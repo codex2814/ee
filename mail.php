15dd205b87136edfdcaee3287f88828102be5cdd
codex2814 <114537636+codex2814@users.noreply.github.com>
2022-10-01 06:00:11 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "youremail@mail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: alexanderrunor.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
